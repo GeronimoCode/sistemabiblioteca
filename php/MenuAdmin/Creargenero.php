7c816fe6c78cc0552431e7babeabe6e3bb2221df
omnittraxxsus <oscarmperez009@gmail.com>
2025-09-29 17:46:20 -0500

    4

--- php/MenuAdmin/Creargenero.php
+++ php/MenuAdmin/Creargenero.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Página principal</title>
-  <link rel="stylesheet" href="../css/inicio.css">
+  <link rel="stylesheet" href="../../css/inicio.css">
   <link rel="icon" href="../img/LogoSanLuis.png">
   <!-- SweetAlert2 -->
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
